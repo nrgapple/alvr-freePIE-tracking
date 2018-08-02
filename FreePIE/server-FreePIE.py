edd0849f493111cc7da23244f1bf8a047a41be04
Adam J. Geiger <ajg5629@arl.psu.edu>
2018-08-02 12:30:56 -0400

    unconnect out ALVR code.

--- FreePIE/server-FreePIE.py
+++ FreePIE/server-FreePIE.py
@@ -12,21 +12,22 @@ def loop():
     data = serverSock.recv(s.size)
     unpacked_data = s.unpack(data)
 
-    #alvr.head_position[0] = unpacked_data[0] * multiplier
-    #alvr.head_position[1] = unpacked_data[1] * multiplier
-    #alvr.head_position[2] = unpacked_data[2] * multiplier
+    alvr.head_position[0] = unpacked_data[0] * multiplier
+    alvr.head_position[1] = unpacked_data[1] * multiplier
+    alvr.head_position[2] = unpacked_data[2] * multiplier
 
-    #alvr.head_orientation[0] = unpacked_data[3]
-    #alvr.head_orientation[1] = unpacked_data[4]
-    #alvr.head_orientation[2] = unpacked_data[5]
-	#diagnostics.debug("x: {0}", str(unpacked_data[0]))
+    alvr.head_orientation[0] = unpacked_data[3]
+    alvr.head_orientation[1] = unpacked_data[4]
+    alvr.head_orientation[2] = unpacked_data[5]
     diagnostics.watch(unpacked_data)
+    diagnostics.watch(alvr.head_position)
+    diagnostics.watch(alvr.head_orientation)
 
 # update -----
 if starting:
 	serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)	
 	serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
-	#alvr.override_head_position = True
-	#alvr.override_head_orientation = True
+	alvr.override_head_position = True
+	alvr.override_head_orientation = True
 
 loop()
