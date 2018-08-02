7e302c41456ec0842563ab1a3b2d04cd71b3db72
Adam J. Geiger <ajg5629@arl.psu.edu>
2018-08-02 12:33:36 -0400

    fix old debug code.

--- FreePIE/server-FreePIE.py
+++ FreePIE/server-FreePIE.py
@@ -2,7 +2,6 @@ import socket
 import sys
 import time
 import struct
-import signal
 
 UDP_IP_ADDRESS = "127.0.0.1"
 UDP_PORT_NO = 6789
@@ -12,9 +11,9 @@ def loop():
     data = serverSock.recv(s.size)
     unpacked_data = s.unpack(data)
 
-    alvr.head_position[0] = unpacked_data[0] * multiplier
-    alvr.head_position[1] = unpacked_data[1] * multiplier
-    alvr.head_position[2] = unpacked_data[2] * multiplier
+    alvr.head_position[0] = unpacked_data[0]
+    alvr.head_position[1] = unpacked_data[1]
+    alvr.head_position[2] = unpacked_data[2]
 
     alvr.head_orientation[0] = unpacked_data[3]
     alvr.head_orientation[1] = unpacked_data[4]
